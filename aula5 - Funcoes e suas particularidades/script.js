3c4626baf9f51ebaa1c626d2be172ed766a3c387
Flavio Milani <frmilanij@gmail.com>
2024-06-03 18:23:57 -0300

    Funcoes e suas particularidades

---
+++ aula5 - Funcoes e suas particularidades/script.js
@@ -0,0 +1,11 @@
+function funcao() {
+    console.log("Funcao sendo chamada");
+}
+
+funcao();
+
+function funcaoComParametro(nome, sobrenome) {
+    console.log(nome + sobrenome + "isso é funcao com parametro sendo chamada");
+}
+
+funcaoComParametro("Maria ", "Cecilia ");
