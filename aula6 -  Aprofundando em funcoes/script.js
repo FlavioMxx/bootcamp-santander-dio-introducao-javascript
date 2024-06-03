e41f336b5713e0283d29c07177ff08b5795439bb
Flavio Milani <frmilanij@gmail.com>
2024-06-03 19:14:08 -0300

    Aprofundando em funcoes

---
+++ aula6 -  Aprofundando em funcoes/script.js
@@ -0,0 +1,19 @@
+// //Tipos de funcao
+
+// // Declarativa
+// function funcao() {
+//     console.log("Chamando funcao declarativa");
+// }
+// funcao();
+
+// // Expressoes de funcoes
+// var funcaoExpressoes = function /*Não é obrigatório nome, js atribui o nome da var*/() {
+//     console.log("Chamando expressao de funcao");
+// }
+// funcaoExpressoes();
+
+// //arrow function
+// var arrowfunction = () => {
+//     console.log("Chamando arrow function");
+// }
+// arrowfunction();
